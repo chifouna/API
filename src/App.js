692d5b34bb5332cc8957f1552a855a2e7c9bfb01
fares larouchi <larouchifares.07@gmail.com>
2021-06-20 21:56:41 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "./component/Header";
+import UserList from "./component/UserList";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <UserList />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/component/Header.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="Header">
+      <div className="Header">
+        <img
+          style={{ margin: "10px" }}
+          width="94px"
+          height="65px"
+          src="./user.jpg"
+          alt="logo"
+        />
+        <p><b>Squad Campany</b></p>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
